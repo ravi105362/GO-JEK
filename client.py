9ab3b1cc78cd3e1f52743894d89a643b7113060f
ravi105362 <ravigupta.mnnit@gmail.com>
2017-02-04 19:55:53 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,19 +1,38 @@
+###############################################################
+#File Name-client.py                                          #
+#Use - Returns to server the number of times ssh done on host #
+#Written By-Ravi Gupta                                        #
+#Date-4/2/2017                                                #
+###############################################################
+
 import time
 import socket               
 
-f=open("auth.log","r")
-line=f.read()
+filePath = 'auth.log'#set here for example actual path /var/log/auth.log
 
-while True :
-	s = socket.socket()        
-	host = socket.gethostname() 
-	port = 12345 
-
-	if "sshd" in line  :
-		print "found"
-		s.connect((host, port))
-		print s.recv(1024)
-		s.send(host)
-		s.shutdown(1)
-		s.close
-	time.sleep(1)
+lastLine = None
+with open(filePath,'r') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            lastLine = line
+#print "lastline is "+lastLine
+while True:
+    with open(filePath,'r') as f:
+        lines = f.readlines()
+        if lines[-1] != lastLine:
+			#print "Last line is not same and line is "+lines[-1]
+			if "sshd" in lines[-1] and "Invalid user" in lines[-1] :
+				#print "found"
+				s = socket.socket()        
+				host = socket.gethostname() 
+				port = 12345 
+				lastLine = lines[-1]
+				print(lines[-1])
+				s.connect((host, port))
+				print s.recv(1024)
+				s.send(host)
+				s.shutdown(1)
+				s.close
+        time.sleep(1)
--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+###########################################################
+#File Name-server.py                                      #
+#Use - Runs a server to count number of clients under ssh #
+#Written By-Ravi Gupta                                    #
+#Date-4/2/2017                                            #
+###########################################################
 import socket               
 
 s = socket.socket()         
@@ -10,7 +16,7 @@ print "Matrix for Login SSH attempts"
 number=0        
 while True:
    c, addr = s.accept()     
-   c.send('Thank you for connecting')
+   c.send('Connected')
    key=c.recv(1024)
    number+=1
    print "*"+ key+" had " +str(number)+ " attempt"
